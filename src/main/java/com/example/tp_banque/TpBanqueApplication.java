e696bf7fa75597e357cdde5829f0a2a4973065c9
AngelVigliengo <angel.vigliengo@limayrac.fr>
2021-12-17 09:42:42 +0100

    Initial commit

---
+++ src/main/java/com/example/tp_banque/TpBanqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp_banque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpBanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpBanqueApplication.class, args);
+	}
+
+}
